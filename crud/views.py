2f389b40b4fc00d9c373ab6da64644b8a6dd1d1b
habibzaka <zakaria.habib@usmba.ac.ma>
2020-11-08 04:41:38 +0100

    djangocrudajax

---
+++ crud/views.py
@@ -0,0 +1,75 @@
+from django.shortcuts import render,get_object_or_404, redirect
+from django.http import JsonResponse
+from django.template.loader import render_to_string
+from django.http import HttpResponse
+# Create your views here.
+from django.contrib.auth.forms import UserCreationForm
+from .forms import *
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from .models import *
+from django.contrib.auth.models import User
+
+def users_list(request):
+	users = User.objects.all()
+	context = {
+	'users': users
+	}
+	return render(request, 'book_list.html',context)
+ 
+
+def users_update(request,id):
+	data = dict()
+	user = User.objects.get(id=id)
+	if request.method == 'POST':
+		form = UserForm(request.POST,instance=user)
+		form.save()
+		data['form_is_valid'] = True
+		users = User.objects.all()
+		data['book_list'] = render_to_string('book_list_2.html',{'users':users})
+	else:
+		form = UserForm(instance=user)
+	 
+	context = {
+	'form':form
+	}
+	data['html_form'] = render_to_string('book_update.html',context,request=request)
+	return JsonResponse(data)
+
+
+def registerPage(request):
+	form = CreateUserForm()
+	if request.method == 'POST':
+		form = CreateUserForm(request.POST)
+		if form.is_valid():
+			form.save()
+			user = form.cleaned_data.get('username')
+			messages.success(request, 'Account was created for ' + user)
+			return redirect('/')
+	context = {'form':form}
+	return render(request, 'register.html', context)
+
+def loginPage(request):
+	
+	if request.method == 'POST':
+		username = request.POST.get('username')
+		password =request.POST.get('password')
+
+		user = authenticate(request, username=username, password=password)
+
+		if user is not None:
+			login(request, user)
+			return redirect('/users')
+		else:
+			messages.info(request, 'Username OR password is incorrect')
+		
+	context = {}
+
+	return render(request, 'login.html', context)
+
+def logoutUser(request):
+	logout(request)
+	return redirect('/')
+
+
+
